4d77a027a29d5da07c6c755cde24f0cd86cd1140
Richaravi <30343892+Richaravi@users.noreply.github.com>
2017-07-22 17:01:04 +0530

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("\nEnter the number=");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("\nThe Given number is even");
+}
+else
+{
+printf("\n The Given number is odd");
+}
+getch();
+}
